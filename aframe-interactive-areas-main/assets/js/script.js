2c531d52499ecd16728b40e7b114d3e6f8e9e3d4
iprojas <rojas.ip@gmail.com>
2022-11-19 19:23:36 +0530

    images

---
+++ aframe-interactive-areas-main/assets/js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('a-scene').addEventListener('loaded', () => {
+        document.querySelector('.preloader').setAttribute('hidden', true)
+    })
+})
+
+AFRAME.registerComponent('person-events-component', {
+    init() {
+        this.resetAnim();
+        this.el.addEventListener('areaEnter', () => {
+            this.startAnim();
+            console.log("Entra al area")
+        })
+        this.el.addEventListener('areaLeave', () => {
+            this.resetAnim();
+        })
+    },
+    startAnim() {
+        this.el.setAttribute('animation-mixer', 'clip:*;')
+    },
+    resetAnim() {
+        this.el.setAttribute('animation-mixer', 'clip:none;')
+    }
+});
